244cce0aee69e7d440d620efe69b1e1b4cb39538
Charan Prasanth L G <charanprasanth2001@gmail.com>
2021-05-24 15:07:21 +0530

    initial commit

---
+++ lib/inbox/Inbox.dart
@@ -0,0 +1,184 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
+import 'package:signal/models/Model.dart';
+
+class Inbox extends StatefulWidget {
+  @override
+  _InboxState createState() => _InboxState();
+}
+
+class _InboxState extends State<Inbox> {
+  List<ChatMessage> messages = [
+    ChatMessage(messageContent: "hello, bro", messageType: "receiver"),
+    ChatMessage(messageContent: "hi, bro", messageType: "sender"),
+    ChatMessage(messageContent: "how are you doing", messageType: "receiver"),
+    ChatMessage(messageContent: "I am fine, bro", messageType: "sender"),
+    ChatMessage(messageContent: "What about you bro?", messageType: "receiver"),
+    ChatMessage(messageContent: "Im fine here!", messageType: "sender"),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color(0xFF1EA955),
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back),
+            onPressed: () {
+              Navigator.pop(context);
+              //print(messages);
+            },
+          ),
+          title: Row(
+            children: [
+              CircleAvatar(
+                backgroundColor: Colors.white,
+                child: Text(
+                  "SA",
+                  style: TextStyle(fontSize: 10),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 10),
+                child: Text(
+                  "Samuel",
+                  style: TextStyle(fontSize: 14),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 2, bottom: 5),
+                child: Icon(
+                  EvilIcons.user,
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+          actions: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(right: 20.0),
+              child: GestureDetector(
+                onTap: () {},
+                child: Icon(
+                  Ionicons.ios_videocam,
+                  size: 30.0,
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(right: 20.0),
+              child: GestureDetector(
+                onTap: () {},
+                child: Icon(
+                  FontAwesome.phone,
+                  size: 30.0,
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(right: 20.0),
+              child: GestureDetector(
+                onTap: () {},
+                child: Icon(
+                  Icons.more_vert,
+                  size: 30.0,
+                ),
+              ),
+            ),
+          ],
+        ),
+        body: Stack(
+          children: <Widget>[
+            ListView.builder(
+              itemCount: messages.length,
+              shrinkWrap: true,
+              padding: EdgeInsets.only(top: 10, bottom: 10),
+              physics: NeverScrollableScrollPhysics(),
+              itemBuilder: (context, index) {
+                return Container(
+                  padding: EdgeInsets.only(
+                      left: 14.0, right: 14, top: 10, bottom: 10.0),
+                  child: Align(
+                    alignment: (messages[index].messageType == "receiver"
+                        ? Alignment.topLeft
+                        : Alignment.topRight),
+                    child: Container(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: messages[index].messageType == "receiver"
+                              ? Color(0xFF1EA955)
+                              : Colors.grey.shade200),
+                      padding: EdgeInsets.all(16),
+                      child: Text(
+                        messages[index].messageContent,
+                        style: TextStyle(
+                            fontSize: 13,
+                            color: messages[index].messageType == "receiver"
+                                ? Colors.white
+                                : Colors.black87),
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: Container(
+                padding: EdgeInsets.only(left: 10.0, bottom: 10.0, top: 10.0),
+                height: 60,
+                width: double.infinity,
+                color: Colors.white,
+                child: Row(
+                  children: <Widget>[
+                    GestureDetector(
+                      onTap: () {},
+                      child: Container(
+                        height: 30,
+                        width: 30,
+                        decoration: BoxDecoration(
+                          color: Color(0xFF1EA955),
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        child: Icon(
+                          Icons.add,
+                          color: Colors.white,
+                          size: 20,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 15,
+                    ),
+                    Expanded(
+                      child: TextField(
+                        decoration: InputDecoration(
+                            hintText: "Write a message",
+                            hintStyle: TextStyle(color: Colors.black54),
+                            border: InputBorder.none),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 15,
+                    ),
+                    FloatingActionButton(
+                      onPressed: () {},
+                      child: Icon(
+                        Icons.send,
+                        color: Colors.white,
+                        size: 18,
+                      ),
+                      backgroundColor: Color(0xFF1EA955),
+                      elevation: 0,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
